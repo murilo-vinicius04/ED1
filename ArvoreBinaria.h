f5b389e37de53c68a33906a6911b2245f7806a11
murilo-vinicius04 <139052152+murilo-vinicius04@users.noreply.github.com>
2024-11-27 21:15:58 -0300

    Create ArvoreBinaria.h

---
+++ ArvoreBinaria.h
@@ -0,0 +1,36 @@
+//  Arquivo ArvBinaria.h
+//  TAD: ProjArvoreBinaria-V2
+
+#define FALSO      0
+#define VERDADEIRO 1
+
+#define OK         1
+#define ERRO       0
+
+typedef int Tipo_Dado;
+
+//Definição do tipo Arvore
+struct NO{
+    Tipo_Dado info;
+    struct NO *esq;
+    struct NO *dir;
+};
+
+typedef struct NO NodoArvBin;
+typedef struct NO *ArvBin;
+
+ArvBin* cria_ArvBin();
+void libera_ArvBin(ArvBin *raiz);
+int insere_ArvBin(ArvBin* raiz, Tipo_Dado valor);
+int remove_ArvBin(ArvBin *raiz, Tipo_Dado valor);
+int estaVazia_ArvBin(ArvBin *raiz);
+int altura_ArvBin(ArvBin *raiz);
+int totalNO_ArvBin(ArvBin *raiz);
+int consulta_ArvBin(ArvBin *raiz, Tipo_Dado valor);
+void preOrdem_ArvBin(ArvBin *raiz);
+void emOrdem_ArvBin(ArvBin *raiz);
+void posOrdem_ArvBin(ArvBin *raiz);
+
+// Funcoes Adicionais
+void Procura_preOrdem_ArvBin(ArvBin *raiz, Tipo_Dado valor, int *achou);
+void Exibe_emOrdem_ArvBin(ArvBin *raiz);
